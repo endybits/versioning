b8282d180aaef482452f79ba265de86663d3be5b
Endy B <endyb.dev@gmail.com>
2023-10-06 22:53:37 +1100

    Test get current version

--- versioning.sh
+++ versioning.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -l
 
 echo "Versioning Type: $1"
+default_version='v0.1.0'
 
-version='v0.5.0'
-
-echo "new_version=$version" >> $GITHUB_OUTPUT
+git fetch --prune --unshalow 2>/dev/null
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+echo 'CURRENT_VERSION $CURRENT_VERSION'
+if [[ $CURRENT_VERSION == '']]; then
+    CURRENT_VERSION=$default_version
+fi
+echo "new_version=$CURRENT_VERSION" >> $GITHUB_OUTPUT
