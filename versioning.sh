baeb33d08b545987c75c63a7c1c08a20388a6f4e
Endy B <endyb.dev@gmail.com>
2023-10-07 15:36:39 +1100

    fix versioning.sh

--- versioning.sh
+++ versioning.sh
@@ -3,9 +3,9 @@
 echo "Versioning Type: $1"
 default_version='v0.1.0'
 
-git logs --oneline
+git log --oneline
 
-git fetch --prune --unshalow 2>/dev/null
+git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 echo 'CURRENT_VERSION $CURRENT_VERSION'
 if [[ $CURRENT_VERSION == '']]; then
