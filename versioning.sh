443f5baa151ebc498145e7273187f13c81e6b7ae
Endy B <endyb.dev@gmail.com>
2023-10-07 16:04:47 +1100

    feat versioning.sh

--- versioning.sh
+++ versioning.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
-echo "Versioning Type: $1"
-default_version='v0.1.0'
+versioning_type=$1
+echo "Versioning Type: $versioning_type"
+initial_version='v0.1.0'
 
 git log --oneline
 
@@ -9,6 +10,23 @@ git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 echo 'CURRENT_VERSION $CURRENT_VERSION'
 if [[ $CURRENT_VERSION == '']]; then
-    CURRENT_VERSION=$default_version
+    CURRENT_VERSION=$initial_version
+    echo "new_version_tag=$CURRENT_VERSION" >> $GITHUB_OUTPUT
+else
+    CURRENT_VERSION_SEGMENTS = (${CURRENT_VERSION//./ })
+    major_current_number = $CURRENT_VERSION_SEGMENTS[0]
+    minor_current_number = $CURRENT_VERSION_SEGMENTS[1]
+    patch_current_number = $CURRENT_VERSION_SEGMENTS[2]
+    if [[ versioning_type == 'patch']]; then
+        patch_current_number=$(( patch_current_number+1 ))
+    elif [[ versioning_type == 'minor']]; then
+        minor_current_number=$(( minor_current_number+1 ))
+    elif [[ versioning_type == 'major']]; then
+        major_current_number=$(( major_current_number+1 ))
+    else
+        echo "No version type SEMVER"
+        exit 1
+    fi
+    NEW_VERSION_TAG="$major_current_number.$minor_current_number.$patch_current_number"
+    echo "new_version_tag=$NEW_VERSION_TAG" >> $GITHUB_OUTPUT
 fi
-echo "new_version=$CURRENT_VERSION" >> $GITHUB_OUTPUT
